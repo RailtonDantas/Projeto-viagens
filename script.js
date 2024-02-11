23cf8e77d36dfe931ab107c39157152b79ef850c
Railton Dantas <155496471+RailtonDantas@users.noreply.github.com>
2024-02-11 10:57:12 -0200

    correção de bugs

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 
+let tag_menu = document.getElementsByTagName("menu")[0]
+let header = document.getElementsByTagName("header")[0]
+let main = document.getElementsByTagName("main")[0]
 function abrir_menu(){
     let menu = document.getElementById("menu1")
-    let tag_menu = document.getElementsByTagName("menu")[0]
-    let header = document.getElementsByTagName("header")[0]
-    let main = document.getElementsByTagName("main")[0]
-    if(tag_menu.style.display == "none"){
+    if(tag_menu.style.display == "none" ){
        tag_menu.style.display = "block"
        header.style.height = "250px"
        tag_menu.style.translate = "0px -100px"
@@ -15,5 +15,19 @@ function abrir_menu(){
         tag_menu.style.display = "none"
         header.style.height = "180px"
         main.style.translate = "0px 0px"
+    }
+}
+
+function mudartamanho(){
+    if(window.innerWidth >= 730){
+        tag_menu.style.display = "block"
+        header.style.height = "250px"
+        main.style.translate = "0px -10px"
+        tag_menu.style.translate = "0px -35px"
+    }
+    else{
+        tag_menu.style.display = "none"
+        header.style.height = "180px"
+        main.style.translate = "0px 0px"  
     }
 }
